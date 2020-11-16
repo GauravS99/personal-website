0ee3c43bf767a81d5294f9628be5d23aed55605c
Gaurav Sharma <g.sharma0909@gmail.com>
2020-11-16 00:28:25 -0500

    changed image extensions

--- src/models/profile.ts
+++ src/models/profile.ts
@@ -10,7 +10,7 @@ export interface ProfileI {
 
 export const dummyProfile : ProfileI = {
     name: "Gaurav Sharma",
-    pfp_src: "https://www.dropbox.com/s/wdb6gbebgnonqhk/profile.PNG?raw=1",
+    pfp_src: "https://previews.dropbox.com/p/thumb/AA-evSVsQxXQNRJ09iFLBgHT5bX9qVbRZwVjPkQUPJonZwCLTXhVwjFEYeQaYrXtnOtNrCJ9CRNBnmypwU0g4r5n7ZSFIx0luhMIO736C56Qx8ZIJdJmve59vXbaC7UFt5dMpBpupTa7TUeJhgAh-nzdY4VeYg0gcr7Bryn5ge4ncecn06Bnup97scxx18GL5ws2b0mVn709sX6j8tjFaV9TbUvY0QWFqruG6-GGv_-YY6QRV3sXNdE3ODbI9cES9Y9ICVES2Pv65ix624hmCfxiUgNznEkfX9HR2s0-dtn3Nl6ljyc0KozH_1pRPRFBrvHkkJPZMYFY7Tdt5SeF4m1W3wCEel-_XGbARlzRMGYNq2Wexy-1GxGBOK0IuAZh2wY/p.png",
     email: "g.sharma0909@gmail.com",
     phone: "+1 647-927-4338",
     github: "https://github.com/GauravS99",
